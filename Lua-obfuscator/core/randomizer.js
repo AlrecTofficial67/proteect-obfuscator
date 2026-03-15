47a32aa16bc7375c9f528b465b7dedc7817a40de
AlrecTofficial67 <239145936+AlrecTofficial67@users.noreply.github.com>
2026-03-15 11:55:43 +0800

    Update randomizer.js

--- Lua-obfuscator/core/randomizer.js
+++ Lua-obfuscator/core/randomizer.js
@@ -1,15 +1,13 @@
 'use strict';
 class Randomizer {
-  constructor(seed) { this.seed = (seed || (Math.floor(Math.random()*0xFFFFFFFF)))>>>0; }
+  constructor(seed) { this.seed=(seed||(Math.floor(Math.random()*0xFFFFFFFF)))>>>0; }
   next() { let s=this.seed; s^=s<<13; s^=s>>17; s^=s<<5; this.seed=s>>>0; return this.seed/0x100000000; }
   nextInt(a,b) { return a+Math.floor(this.next()*(b-a+1)); }
   randomName(min,max) {
     const a='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const n='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_';
-    const len=this.nextInt(min||8,max||16);
-    let s=a[this.nextInt(0,a.length-1)];
-    for(let i=1;i<len;i++) s+=n[this.nextInt(0,n.length-1)];
-    return s;
+    const len=this.nextInt(min||8,max||16); let s=a[this.nextInt(0,a.length-1)];
+    for(let i=1;i<len;i++) s+=n[this.nextInt(0,n.length-1)]; return s;
   }
   randomKeyArray(len) { const k=[]; for(let i=0;i<len;i++) k.push(this.nextInt(1,254)); return k; }
   shuffle(arr) { for(let i=arr.length-1;i>0;i--){const j=this.nextInt(0,i);[arr[i],arr[j]]=[arr[j],arr[i]];} return arr; }
