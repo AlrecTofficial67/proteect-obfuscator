057f8d1b1a6919f3a4b7ad537d8dc24c2674f6d4
AlrecTofficial67 <239145936+AlrecTofficial67@users.noreply.github.com>
2026-03-15 10:21:38 +0800

    Update randomizer.js

--- Lua-obfuscator/core/randomizer.js
+++ Lua-obfuscator/core/randomizer.js
@@ -1,45 +1,18 @@
 'use strict';
-
 class Randomizer {
-  constructor(seed) {
-    this.seed = (seed || (Math.floor(Math.random() * 0xFFFFFFFF))) >>> 0;
+  constructor(seed) { this.seed = (seed || (Math.floor(Math.random()*0xFFFFFFFF)))>>>0; }
+  next() { let s=this.seed; s^=s<<13; s^=s>>17; s^=s<<5; this.seed=s>>>0; return this.seed/0x100000000; }
+  nextInt(a,b) { return a+Math.floor(this.next()*(b-a+1)); }
+  randomName(min,max) {
+    const a='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const n='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_';
+    const len=this.nextInt(min||8,max||16);
+    let s=a[this.nextInt(0,a.length-1)];
+    for(let i=1;i<len;i++) s+=n[this.nextInt(0,n.length-1)];
+    return s;
   }
-
-  next() {
-    let s = this.seed;
-    s ^= s << 13; s ^= s >> 17; s ^= s << 5;
-    this.seed = s >>> 0;
-    return this.seed / 0x100000000;
-  }
-
-  nextInt(min, max) {
-    return min + Math.floor(this.next() * (max - min + 1));
-  }
-
-  randomName(minLen, maxLen) {
-    const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const alnum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_';
-    const len = this.nextInt(minLen || 10, maxLen || 20);
-    let name = alpha[this.nextInt(0, alpha.length - 1)];
-    for (let i = 1; i < len; i++) name += alnum[this.nextInt(0, alnum.length - 1)];
-    return name;
-  }
-
-  randomKeyArray(len) {
-    const k = [];
-    for (let i = 0; i < len; i++) k.push(this.nextInt(1, 254));
-    return k;
-  }
-
-  shuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = this.nextInt(0, i);
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-  }
-
-  pick(arr) { return arr[this.nextInt(0, arr.length - 1)]; }
+  randomKeyArray(len) { const k=[]; for(let i=0;i<len;i++) k.push(this.nextInt(1,254)); return k; }
+  shuffle(arr) { for(let i=arr.length-1;i>0;i--){const j=this.nextInt(0,i);[arr[i],arr[j]]=[arr[j],arr[i]];} return arr; }
+  pick(arr) { return arr[this.nextInt(0,arr.length-1)]; }
 }
-
 module.exports = { Randomizer };
